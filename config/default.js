0a8c73af31eb0d4bbef4a56c499a1f3418d2d973
Sergio <nadokon@gmail.com>
2018-01-12 00:46:19 +0200

    stratum init

---
+++ config/default.js
@@ -0,0 +1,21 @@
+module.exports = {
+  host: "pool.supportxmr.com",
+  port: 3333,
+  pass: "x",
+  ssl: false,
+  address: null,
+  user: null,
+  diff: null,
+  dynamicPool: false,
+  maxMinersPerConnection: 100,
+  donations: [
+    {
+      address: "48raaL2TG5oddQR5rHgEJDTGbb94kXRBncm1kKruppikTJRsq3s8eZK4gvDYFK8LYAZ96ADJtq5FDVYFJMzfZ1xRDrwv7d8",
+      host: "pool.supportxmr.com",
+      port: 3333,
+      user: null,
+      pass: "donations",
+      percentage: 0.01 // 1%
+    }
+  ]
+};
